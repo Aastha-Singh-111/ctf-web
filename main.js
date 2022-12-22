6704a5c97897353a213624e4c5afc5bcc31e5ee3
Aastha-Singh-111 <98797127+Aastha-Singh-111@users.noreply.github.com>
2022-12-23 02:34:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// const mobile_nav = document.getElementById("mobnavid");
+// const nav_header = document.getElementById("smallNav");
+// // console.log(mobile_nav);
+// // console.log(nav_header);
+// function toggleNavbar(){
+//     console.log(23)
+//     console.log("hello world");
+//     // alert("plz here");
+//     nav_header.classList.add("active");
+//     console.log(24);
+    
+// }
+
+// function toggleNavbar2(){
+//     console.log(23)
+//     console.log("hello world");
+//     // alert("plz here");
+//     nav_header.classList.remove("active");
+//     console.log(24);
+    
+// }
+
+
+
+const mobile_nav = document.getElementById("mobnavid");
+const nav_header = document.getElementById("smallNav");
+// console.log(mobile_nav);
+// console.log(nav_header);
+function toggleNavbar(){
+    console.log(23)
+    console.log("hello world");
+    // alert("plz here");
+    nav_header.classList.add("active");
+    console.log(24);
+    
+}
+
+function toggleNavbar2(){
+    console.log(23)
+    console.log("hello world");
+    // alert("plz here");
+    nav_header.classList.remove("active");
+    console.log(24);
+    
+}
+// mobile_nav.addEventListener("click",() => toggleNavbar());
+
+// document.getElementsByClassName("mobilenav").onclick
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// mobile_nav.addEventListener("click",() => toggleNavbar());
+
+// document.getElementsByClassName("mobilenav").onclick
